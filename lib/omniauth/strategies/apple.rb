aac6d918b601c72abcd2a576c236ad9b652e9196
Nicholas Busby <Nicholas.Busby@Live.com>
2020-08-04 11:24:01 -0700

    Revert "adding in code based on stackoverflow"
    This reverts commit c20f3ee31ff688dd9302f79a26bb16595cd22533.

--- lib/omniauth/strategies/apple.rb
+++ lib/omniauth/strategies/apple.rb
@@ -15,7 +15,7 @@ module OmniAuth
              token_url: '/auth/token'
       option :authorize_params,
              response_mode: 'form_post',
-             response_type: 'code id_token'
+             response_type: 'id_token'
 
       uid { id_info['sub'] }
 
